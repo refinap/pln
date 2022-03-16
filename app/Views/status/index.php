6bd7b337469487b63aaf7eca0fef7d42cff72066
Farhan-zawahir <94586034+Farhan-zawahir@users.noreply.github.com>
2022-03-16 08:03:53 +0700

    index tanggal 16 maret 2022 jam 8

--- app/Views/status/index.php
+++ app/Views/status/index.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-< <div class="container">
+<div class="container">
     <div class="row">
         <div class="col">
             <h1 class="text-center mt-2">Monitoring Realtime SCADA</h1>
@@ -13,39 +13,40 @@
                     <div class="col">
                         <div class="collapse" class="btn-group" id="data-<?= $key + 1 ?>">
                             <div class="card card-body">
+
                                 <?php foreach ($a->gi as $g) : ?>
-                                    <p class="fw-bold fs-4"><?php echo $g['GARDU_INDUK_NAMA'] . "<br>"; ?></p>
+                                    <span class="fw-bold fs-4"><?php echo $g['GARDU_INDUK_NAMA']; ?></span>
                                     <ul>
                                         <?php foreach ($g['incoming'] as $income) : ?>
-                                            <li>
-                                                <p class="fw-bold fs-5"><?php echo $income['NAMA_ALIAS_INCOMING']; ?></p>
-                                            </li>
-                                            <?php foreach ($income['cubicle'] as $cubic) : ?>
-                                                <?php if ($cubic['SCB'] === '1') : ?>
-                                                    <div class="btn-group-vertical">
-                                                        <p class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></p>
-                                                        <button type="button" class="btn btn-danger">Close</button>
+                                            <span class="fw-bold fs-5"><?php echo $income['NAMA_ALIAS_INCOMING']; ?></span>
+                                            <div class="d-flex flex-row bd-highlight">
+
+                                                <?php foreach ($income['cubicle'] as $cubic) : ?>
+                                                    <?php
+                                                    if ($cubic['SCB'] === '1') {
+                                                        $arr = array('danger', 'Close');
+                                                    } elseif ($cubic['SCB'] === '0') {
+                                                        $arr = array('success', 'Open');
+                                                    } elseif ($cubic['SCB'] === NULL) {
+                                                        $arr = array('warning', 'Cadangan');
+                                                    } else {
+                                                        $arr = array('dark', 'invalid');
+                                                    }
+                                                    ?>
+
+                                                    <div class="p-2 d-grid gap-2" style="min-width:120px;">
+                                                        <div class="d-flex justify-content-center">
+                                                            <span class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></span>
+                                                        </div>
+                                                        <button type="button" class="btn btn-<?= $arr[0]; ?>"><?= $arr[1]; ?></button>
                                                     </div>
-                                                <?php elseif ($cubic['SCB'] === '0') : ?>
-                                                    <div class="btn-group-vertical">
-                                                        <p class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></p>
-                                                        <button type="button" class="btn btn-success">Open</button>
-                                                    </div>
-                                                <?php elseif ($cubic['SCB'] === NULL) : ?>
-                                                    <div class="btn-group-vertical">
-                                                        <p class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></p>
-                                                        <button type="button" class="btn btn-warning">Cadangan</button>
-                                                    </div>
-                                                <?php else : ?>
-                                                    <div class="btn-group-vertical">
-                                                        <p class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></p>
-                                                        <button type="button" class="btn btn-dark">invalid</button>
-                                                    </div>
-                                                <?php endif; ?>
-                                            <?php endforeach; ?>
+
+
+
+                                                <?php endforeach; ?>
+                                            </div>
                                         <?php endforeach; ?>
                                     </ul>
-
                                 <?php endforeach; ?>
                             </div>
                         </div>
@@ -54,5 +55,5 @@
             <?php endforeach; ?>
         </div>
     </div>
-    </div>
-    <?= $this->endSection(); ?>
+</div>
+<?= $this->endSection(); ?>
