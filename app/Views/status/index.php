f61cc9aff5391620cca58946d588d794f6f185b1
Farhan-zawahir <94586034+Farhan-zawahir@users.noreply.github.com>
2022-03-17 15:06:11 +0700

    Update index tabel informasi tambahan 17 maret 2022

--- app/Views/status/index.php
+++ app/Views/status/index.php
@@ -6,9 +6,10 @@
         <div class="col">
             <h1 class="text-center mt-2">Monitoring Realtime SCADA</h1>
             <h2 class="text-center mt-2">Unit Pelaksana Pelayanan Pelanggan (UP3)</h2>
+
             <a href="/status/tambah" class="btn btn-primary mb-3">Tambah Data</a><br>
             <?php foreach ($apj as $key => $a) : ?>
-                <button class="btn btn-primary mb-1" data-bs-toggle="collapse" data-bs-target="#data-<?= $key + 1 ?>" aria-expanded="false" aria-controls="data-<?= $key + 1 ?>"> <?= $a->APJ_NAMA ?></button>
+                <button class="btn btn-primary mb-1" data-bs-toggle="collapse" data-bs-target="#data-<?= $key + 1 ?>" style="min-width:150px;" aria-expanded="false" aria-controls="data-<?= $key + 1 ?>" class="p-2 d-grid gap-2" style="min-width:120px;"> <?= $a->APJ_NAMA ?></button>
                 <div class="collapse multi-collapse" class="btn-group" id="data-<?= $key + 1 ?>">
                     <div class="card card-body">
                         <?php foreach ($a->gi as $g) : ?>
@@ -33,7 +34,61 @@
                                                 <div class="d-flex justify-content-center">
                                                     <span class="fs-5"><?= $cubic['CUBICLE_NAME']; ?></span>
                                                 </div>
-                                                <button type="button" class="btn btn-<?= $arr[0]; ?>"><?= $arr[1]; ?></button>
+                                                <button type="button" data-bs-toggle="modal" data-bs-target="#myModal" class="btn btn-<?= $arr[0]; ?>"><?= $arr[1]; ?></button>
+
+                                                <!-- The Modal -->
+                                                <div class="modal fade" id="myModal">
+                                                    <div class="modal-dialog modal-lg">
+                                                        <div class="modal-content">
+
+                                                            <!-- Modal Header -->
+                                                            <div class="modal-header">
+                                                                <h4 class="modal-title">Informasi</h4>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                                            </div>
+
+                                                            <!-- Modal body -->
+                                                            <div class="modal-body">
+
+                                                                <div class="container mt-3">
+                                                                    <table class="table table-bordered">
+                                                                        <thead>
+                                                                            <tr>
+                                                                                <th>Keterangan</th>
+                                                                                <th>Nilai</th>
+                                                                                <th>Waktu</th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            <tr>
+                                                                                <td>IA</td>
+                                                                                <td>84</td>
+                                                                                <td>2022-03-02 15:34:00</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td>IB</td>
+                                                                                <td>95</td>
+                                                                                <td>2022-03-02 15:34:00</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td>IC</td>
+                                                                                <td>124</td>
+                                                                                <td>2022-03-02 15:34:00</td>
+                                                                            </tr>
+                                                                        </tbody>
+                                                                    </table>
+                                                                </div>
+                                                            </div>
+
+                                                            <!-- Modal footer -->
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                             </div>
                                         <?php endforeach; ?>
                                     </div>
