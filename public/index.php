3b84b6a16b45575979ec0ebd56ca8edb46226396
Refin Ananda <anandarefin@gmail.com>
2022-07-18 08:16:16 +0700

    update  pathConfig index.php public

--- public/index.php
+++ public/index.php
@@ -17,7 +17,9 @@ chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
+//pilih satu
 $pathsConfig = FCPATH . '../app/Config/Paths.php';
+// $pathsConfig = FCPATH . '../ci4_dash20kv/app/Config/Paths.php';
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
 
